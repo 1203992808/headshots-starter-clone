de8424e8e871700aa56c4b355b4c9242a0a88610
1203992808@qq.com <1203992808@qq.com>
2025-03-25 19:56:53 +0800

    fix: update webhook URLs to use production domain

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -147,10 +147,10 @@ export async function POST(request: Request) {
 
   try {
 
-    const trainWebhook = `https://${process.env.VERCEL_URL}/astria/train-webhook`;
+    const trainWebhook = `https://headshots-starter-clone-sigma-blond.vercel.app/astria/train-webhook`;
     const trainWebhookWithParams = `${trainWebhook}?user_id=${user.id}&model_id=${modelId}&webhook_secret=${appWebhookSecret}`;
 
-    const promptWebhook = `https://${process.env.VERCEL_URL}/astria/prompt-webhook`;
+    const promptWebhook = `https://headshots-starter-clone-sigma-blond.vercel.app/astria/prompt-webhook`;
     const promptWebhookWithParams = `${promptWebhook}?user_id=${user.id}&&model_id=${modelId}&webhook_secret=${appWebhookSecret}`;
 
     const API_KEY = astriaApiKey;
