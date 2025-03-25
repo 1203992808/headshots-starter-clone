ac8df48c54a4c989c2d8d33e455f90a613212ddc
1203992808@qq.com <1203992808@qq.com>
2025-03-25 20:43:17 +0800

    fix: update Next.js config to support dynamic rendering

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
+  // Allow JSX/JS files to import each other
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // Disable static optimization to force dynamic rendering
+  staticPageGenerationTimeout: 1000,
 };
 
 module.exports = nextConfig;
